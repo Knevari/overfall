f4f8600edb7bb233b74803f48cd32cc5f6a16611
Knevari <mateus7319@gmail.com>
2019-09-20 15:05:18 -0300

    Adding an example (trying)

---
+++ examples/movies_example.js
@@ -0,0 +1,35 @@
+const Overfall = require('../src');
+
+const fakeDatabase = {
+  movies: [
+    {
+      id: 1,
+      name: "Lord of The Rings"
+    }
+  ],
+  update(key, values) {
+    this[key].push(...values);
+  }
+}
+
+const overfall = new Overfall({ movies: [] });
+// overfall.state.movies.syncWith(fakeDatabase.movies);
+
+overfall.persistState();
+
+overfall.on("update_movies").do(({ movies }) => {
+  fakeDatabase.update("movies", movies);
+}).when("movies");
+
+overfall.changeState(previous => ({
+  movies: [...previous.movies, { id: 2, name: "Harry Potter" }]
+}));
+
+overfall.events.update_movies.delete();
+
+// Possible future APIs
+// - overfall.loadFromStorage()
+// - overfall.loadStateFrom(obj)
+// - overfall.state[prop].syncWith()
+
+// It could return only the data that was added to state.
